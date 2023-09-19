ac3e8398a721addd0aefd59a9f230d3b5424dcec
adolforf98 <adolrfo@gmail.com>
2023-09-19 19:39:38 +0200

    Cambios intermedios

--- Calculadora/src/calculadorasimple/Main.java
+++ Calculadora/src/calculadorasimple/Main.java
@@ -21,48 +21,64 @@ public class Main {
 			System.out.println("2. Restar");
 			System.out.println("3. Multiplicar");
 			System.out.println("4. Dividir");
+			System.out.println("5. Calculo Raices");
+			System.out.println("6. Calculo Potencias");
 			System.out.println("Pulse 9 para salir");
 			
 			try {
 				opcion = input.nextInt();
 			} catch (InputMismatchException ex) {
-	            System.out.println("Debe ingresar obligatoriamente un número entero.");
+	            System.out.println("Debe ingresar obligatoriamente un numero entero.");
 	        break;
 	        }
 			
 				switch(opcion) {
 		  case 1:
-		    System.out.println("Introduzca el primer numero");
+		    System.out.println("Introduzca el primer sumando");
 		    numero1 = input.nextDouble();
 		    
-		    System.out.println("Introduzca el primer numero");
+		    System.out.println("Introduzca el segundo sumando");
 		    numero2 = input.nextDouble();
 		    calculadora.Sumar(numero1, numero2);
 		    
 		    break;
 		  case 2:
-			    System.out.println("Introduzca el primer numero");
+			    System.out.println("Introduzca el minuendo");
 			    numero1 = input.nextDouble();
-			    System.out.println("Introduzca el primer numero");
+			    System.out.println("Introduzca el sustraendo");
 			    numero2 = input.nextDouble();
 			    calculadora.Restar(numero1, numero2);
 		    break;
 		  case 3:
-			    System.out.println("Introduzca el primer numero");
+			    System.out.println("Introduzca el primer factor");
 			    numero1 = input.nextDouble();
-			    System.out.println("Introduzca el primer numero");
+			    System.out.println("Introduzca el segundo factor");
 			    numero2 = input.nextDouble();
 			    calculadora.Multiplicar(numero1, numero2);
 		    break;
 		  case 4:
-			    System.out.println("Introduzca el primer numero");
+			    System.out.println("Introduzca el dividendo");
 			    numero1 = input.nextDouble();
-			    System.out.println("Introduzca el primer numero");
+			    System.out.println("Introduzca el divisor");
 			    numero2 = input.nextDouble();
 			    calculadora.Dividir(numero1, numero2);
 		    break;
+		  case 5:
+			    System.out.println("Introduzca el numero");
+			    numero1 = input.nextDouble();
+			    System.out.println("Introduzca la raiz");
+			    numero2 = input.nextDouble();
+			    calculadora.CalculoRaices(numero1, numero2);
+		    break;
+		  case 6:
+			    System.out.println("Introduzca el numero");
+			    numero1 = input.nextDouble();
+			    System.out.println("Introduzca la potencia");
+			    numero2 = input.nextDouble();
+			    calculadora.CalculoPotencias(numero1, numero2);
+		    break;
 		  case 9:
-			  System.out.println("Hasta la próxima!");
+			  System.out.println("Hasta la proxima!");
 			  break;
 		  default:
 		    System.out.println("Introduzca una opcion valida");
--- Calculadora/src/calculadorasimple/calculadora.java
+++ Calculadora/src/calculadorasimple/calculadora.java
@@ -1,39 +1,47 @@
 package calculadorasimple;
 
 public class calculadora {
-	static double resultadosuma;
-	static double resultadoresta;
-	static double resultadodivision;
-	static double resultadomultiplicacion;
+	static double resultado;
 
 	
 	protected static double Sumar(double numero1 , double numero2) {
 		
-		resultadosuma = numero1 + numero2;
-		System.out.println(resultadosuma);
-		return resultadosuma;
+		resultado = numero1 + numero2;
+		System.out.println("El resultado es "+resultado);
+		return resultado;
 	}
 	
 	protected static double Restar(double numero1 , double numero2) {
 		
-		resultadoresta = numero1 - numero2;
-		System.out.println(resultadoresta);
-		return resultadoresta;
+		resultado = numero1 - numero2;
+		System.out.println("El resultado es "+resultado);
+		return resultado;
 	}
 
 	protected static double Multiplicar(double numero1 , double numero2) {
 		
-		resultadomultiplicacion = numero1 * numero2;
-		System.out.println(resultadomultiplicacion);
-		return resultadomultiplicacion;
+		resultado = numero1 * numero2;
+		System.out.println("El resultado es "+resultado);
+		return resultado;
 	}
 	
 	protected static double Dividir(double numero1 , double numero2) {
 		
-		resultadodivision = numero1 / numero2;
-		System.out.println(resultadodivision);
-		return resultadodivision;
-		
+		resultado = numero1 / numero2;
+		System.out.println("El resultado es "+resultado);
+		return resultado;
+	}
+	
+	protected static double CalculoRaices (double numero1, double numero2) {
+		resultado = Math.pow(numero1, 1/numero2);
+		System.out.println("El resultado es "+resultado);
+		return resultado;
+	}
+	
+	protected static double CalculoPotencias (double numero1, double numero2) {
+		resultado = Math.pow(numero1, numero2);
+		System.out.println("El resultado es "+resultado);
+		return resultado;
 	}
 
 }
